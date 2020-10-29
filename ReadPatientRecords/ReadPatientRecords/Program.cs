e5286cdfcdc665b983d0fd66d1d44c09e375cba2
Richard <52008509+richardbr-coder@users.noreply.github.com>
2020-10-29 14:41:12 -0230

    Add files via upload

---
+++ ReadPatientRecords/ReadPatientRecords/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReadPatientRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read and show info from the file.
+            string line = " ";
+
+            try
+            {
+                //text reader
+                using (StreamReader sr = new StreamReader("PatientRecords.txt"))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            //Exception handling
+            catch
+            {
+                Console.WriteLine("ERROR - The file is invalid");
+            }
+            Console.ReadKey();
+        }
+    }
+}
